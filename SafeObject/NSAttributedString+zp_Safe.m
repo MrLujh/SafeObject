350288bd0479455e561df1487dc1f185714fbfea
张朋朋 <944160330@qq.com>
2019-04-10 16:52:10 +0800

    fork 修改- 添加NSAttributedString 和 NSMutableAttributeString

---
+++ SafeObject/NSAttributedString+zp_Safe.m
@@ -0,0 +1,33 @@
+//
+//  NSAttributedString+zp_Safe.m
+//  SafeObjectCrash
+//
+//  Created by admin on 2019/4/10.
+//  Copyright © 2019 lujh. All rights reserved.
+//
+
+#import "NSAttributedString+zp_Safe.h"
+#import <objc/runtime.h>
+
+@implementation NSAttributedString (zp_Safe)
++ (void)load{
+    
+    Method oldMethod = class_getInstanceMethod(self, @selector(initWithString:attributes:));
+    Method safeMethod = class_getInstanceMethod(self, @selector(safe_initWithString:attributes:));
+    
+    method_exchangeImplementations(oldMethod, safeMethod);
+    
+
+}
+- (instancetype)safe_initWithString:(NSString *)str attributes:(NSDictionary<NSAttributedStringKey,id> *)attrs{
+    if (str.length <= 0) {
+#ifdef DEBUG
+        NSAssert(str, @"Method (initWithString:attributes:) invlie:str");
+#else
+         str = @"";
+#endif
+       
+    }
+   return [self safe_initWithString:str attributes:attrs];
+}
+@end
